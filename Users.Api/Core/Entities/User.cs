3ff54b0f634635e389dd6e157bb4524056169a7a
RaffDevs <rveronezdeveloper@gmail.com>
2024-07-24 22:46:28 -0300

    refac: improve folder structure

--- Users.Api/Core/Entities/User.cs
+++ Users.Api/Core/Entities/User.cs
@@ -1,7 +1,7 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace Models
+namespace Users.Api.Core.Entities
 {
     [Table("users")]
     public class User
